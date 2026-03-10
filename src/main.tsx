9f81b2ea2ca9aea6df626ff9744e4e8704ab0e39
shehryar rehman <131074050+shery8595@users.noreply.github.com>
2026-03-11 01:00:06 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { Buffer } from 'buffer';
-import process from 'process';
-
-window.Buffer = Buffer;
-window.process = process;
-
+import './polyfills';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
